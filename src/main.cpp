cce2703eca1274d3f5cdb4bf247df755e3dd5874
Vasiliy Malakhov <mvamgc@gmail.com>
2017-05-21 19:57:29 -0400

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -11,131 +11,37 @@
 
 #include <SimpleTimer.h>
 
+#include <U8g2lib.h>
+
 #define BLYNK_PRINT Serial
 #include <BlynkSimpleEsp8266.h>
 
-#include <U8g2lib.h>
-
-void drawMessage(char const *msg);
-void drawMessageLF(char const *msg);
-void draw(float temp, float humidity, float pressure);
-void espInfo();
-
 #define I2C_SDA 5
 #define I2C_SCL 4
-
-ADC_MODE(ADC_VCC);
-
-char auth[] = "e02bfd9cc86e4c29805a87b707f9a409";
-char server[] = "2010.io";
 
 WiFiManager wifiManager;
 
 BME280I2C bme;
 
+SimpleTimer timer;
+
+// Blynk data
+char auth[] = "e02bfd9cc86e4c29805a87b707f9a409";
+char server[] = "2010.io";
 WidgetTerminal terminal(V10);
 
-bool metric = true;
-uint8_t pressureUnit = 1; // unit: B000 = Pa, B001 = hPa, B010 = Hg, B011 = atm, B100 = bar, B101 = torr, B110 = N/m^2, B111 = psi
-
-SimpleTimer timer;
+// Measured data
 float temp, hum, pres;
 bool bmeReady = false;
 
+// u8g2
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, I2C_SCL, I2C_SDA, U8X8_PIN_NONE);
 byte x {0};
 byte y {0};
 bool screenOn = true;
 
-void sendMeasurements() {
-  bme.read(pres, temp, hum, metric, pressureUnit); // Parameters: (float& pressure, float& temp, float& humidity, bool celsius = false, uint8_t pressureUnit = 0x0)
-  if(screenOn) {
-    draw(temp, hum, pres);
-  }
-  // draw(temp, hum, pres);
 
-  Serial.print("Mes: Temp: ");
-  Serial.print(temp, 4);
-  Serial.print(", Humidity: ");
-  Serial.print(hum, 4);
-  Serial.print(", Pressure: ");
-  Serial.print(pres, 4);
-  Serial.println("");
-  Blynk.virtualWrite(1, temp);
-  // Blynk.virtualWrite(6, temp);
-  Blynk.virtualWrite(2, hum);
-  Blynk.virtualWrite(3, pres);
-}
-
-void setup() {
-  Serial.begin(9600);
-  u8g2.begin();
-
-  drawMessageLF("Start");
-
-  pinMode(BUILTIN_LED, OUTPUT);
-  digitalWrite(BUILTIN_LED, LOW);
-  delay(500);
-  digitalWrite(BUILTIN_LED, HIGH);
-  delay(500);
-  digitalWrite(BUILTIN_LED, LOW);
-  delay(500);
-
-  wifiManager.autoConnect("AutoConnectAP", "esp8266");
-  Serial.println("connected...yeey :)");
-  drawMessage("WiFi Connected");
-
-  while(!bme.begin(I2C_SDA, I2C_SCL)){
-    Serial.println("Could not find BME280 sensor!");
-    // Serial.println(BUILTIN_LED);
-    delay(2000);
-  }
-  drawMessage("BME initialized");
-  digitalWrite(BUILTIN_LED, HIGH);
-  delay(500);
-  digitalWrite(BUILTIN_LED, LOW);
-  delay(500);
-  digitalWrite(BUILTIN_LED, HIGH);
-
-  Blynk.config(auth, server);
-
-  drawMessage("Blynk configured");
-
-  bmeReady = true;
-  timer.setInterval(15000L, sendMeasurements);
-  drawMessage("Timer started");
-  sendMeasurements();
-}
-
-void loop() {
-  Blynk.run();
-  timer.run();
-}
-
-BLYNK_WRITE(V7) {
-  screenOn = param[0].asInt() == 1;
-  if(screenOn) {
-    draw(temp, hum, pres);
-  } else {
-    u8g2.clear();
-  }
-}
-
-BLYNK_WRITE(V10) {
-  const char * cmdc = param[0].asStr();
-  Serial.println("V10 cmd received: ");
-  String cmd = String(cmdc);
-  Serial.println(cmd);
-  if(cmd == "scron") {
-    screenOn = true;
-    draw(temp, hum, pres);
-    terminal.println("Screen on");
-  } else if(cmd == "scroff") {
-    screenOn = false;
-    u8g2.clear();
-    terminal.println("Screen off");
-  }
-}
+// ------------------------- Screen -------------------------
 
 void draw(float temp, float humidity, float pressure) {
   Serial.print("draw: ");
@@ -186,4 +92,105 @@ void drawMessage(char const *msg) {
   x = (128 - u8g2.getStrWidth(msg))/2;
   u8g2.drawStr(x, 30, msg);
   u8g2.sendBuffer();
+}
+
+void drawMeasurements() {
+  if(screenOn) {
+    draw(temp, hum, pres);
+  }
+}
+
+// ------------------------- Blynk -------------------------
+void sendMeasurements() {
+  Blynk.virtualWrite(1, temp);
+  Blynk.virtualWrite(2, hum);
+  Blynk.virtualWrite(3, pres);
+}
+
+BLYNK_WRITE(V7) {
+  screenOn = param[0].asInt() == 1;
+  if(screenOn) {
+    draw(temp, hum, pres);
+  } else {
+    u8g2.clear();
+  }
+}
+
+BLYNK_WRITE(V10) {
+  const char * cmdc = param[0].asStr();
+  Serial.println("V10 cmd received: ");
+  String cmd = String(cmdc);
+  Serial.println(cmd);
+  if(cmd == "scron") {
+    screenOn = true;
+    draw(temp, hum, pres);
+    terminal.println("Screen on");
+  } else if(cmd == "scroff") {
+    screenOn = false;
+    u8g2.clear();
+    terminal.println("Screen off");
+  } else {
+    terminal.println("Supported commands:\nscron\nscroff");
+  }
+  terminal.flush();
+}
+
+// ------------------------- BME280 -------------------------
+void readMeasurements() {
+  uint8_t pressureUnit = 1; // unit: B000 = Pa, B001 = hPa, B010 = Hg, B011 = atm, B100 = bar, B101 = torr, B110 = N/m^2, B111 = psi
+  bme.read(pres, temp, hum, true, pressureUnit); // Parameters: (float& pressure, float& temp, float& humidity, bool celsius = false, uint8_t pressureUnit = 0x0)
+  Serial.print("Mes: Temp: ");
+  Serial.print(temp, 4);
+  Serial.print(", Humidity: ");
+  Serial.print(hum, 4);
+  Serial.print(", Pressure: ");
+  Serial.print(pres, 4);
+  Serial.println("");
+  sendMeasurements();
+  drawMeasurements();
+}
+
+void setup() {
+  Serial.begin(9600);
+  u8g2.begin();
+
+  drawMessageLF("Start");
+
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(500);
+  digitalWrite(BUILTIN_LED, HIGH);
+  delay(500);
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(500);
+
+  wifiManager.autoConnect("AutoConnectAP", "esp8266");
+  Serial.println("connected...yeey :)");
+  drawMessage("WiFi Connected");
+
+  while(!bme.begin(I2C_SDA, I2C_SCL)){
+    Serial.println("Could not find BME280 sensor!");
+    // Serial.println(BUILTIN_LED);
+    delay(2000);
+  }
+  drawMessage("BME initialized");
+  digitalWrite(BUILTIN_LED, HIGH);
+  delay(500);
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(500);
+  digitalWrite(BUILTIN_LED, HIGH);
+
+  Blynk.config(auth, server);
+
+  drawMessage("Blynk configured");
+
+  bmeReady = true;
+  timer.setInterval(15000L, readMeasurements);
+  drawMessage("Timer started");
+  readMeasurements();
+}
+
+void loop() {
+  Blynk.run();
+  timer.run();
 }
